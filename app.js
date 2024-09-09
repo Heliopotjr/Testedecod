df3b15e3c0b27ba1fcaf2e145e6e79d4d5959f0a
Heliopotjr <60743218+Heliopotjr@users.noreply.github.com>
2024-09-08 22:33:38 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+function pesquisar() {
+    // Obtém o elemento HTML onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+    
+    // Obtém o valor digitado no campo de pesquisa e o converte para letras minúsculas
+    // Isso garante que a pesquisa não seja sensível a maiúsculas/minúsculas
+    let campoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase();
+
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+
+    // Loop através de todos os itens do array 'dados'
+    for (let dado of dados) {
+        // Verifica se o ranking ou a descrição do game contém o termo pesquisado
+        // Usamos `toLowerCase()` para fazer uma comparação insensível a maiúsculas/minúsculas
+        if (dado.ranking.toLowerCase().includes(campoPesquisa) || dado.descricao.toLowerCase().includes(campoPesquisa)) {
+            // Se o termo for encontrado, cria-se um bloco de HTML contendo as informações do game
+            resultados += `
+            <div class="item-resultado">
+                 <h3 class="ranking">${dado.ranking}</h3> <!-- Exibe a colocação no ranking -->
+                <h2>
+                    <a href="${dado.link}" target="_blank">${dado.titulo}</a> <!-- Link com o título do game -->
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p> <!-- Descrição do game -->
+                <a href="${dado.link}" target="_blank">Mais informações</a> <!-- Link para mais informações -->
+            </div>
+            `;
+        }
+    }
+
+    // Caso nenhum resultado corresponda ao termo de pesquisa, exibe uma mensagem de "Nenhum game encontrado"
+    if (resultados === "") {
+        resultados = "<p>Nenhum game encontrado.</p>";
+    }
+
+    // Insere os resultados gerados (ou a mensagem de "Nenhum game encontrado") dentro do elemento HTML correspondente
+    section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,31 @@
+let dados = [{
+    titulo: "Astro bot",
+    descricao: "Astro's Playroom tem sido um dos melhores — e gratuitos! — jogos para consoles PS5 por anos, e com Astro Bot, a Team Asobi consolidou sua posição como um dos melhores estúdios first-party da Sony. Uma aventura emocionante que é encantadora sem esforço, Astro Bot parece consistentemente divertido de jogar graças ao seu forte núcleo de plataforma, uma montanha de segredos e uso excepcional do controle DualSense que faz você se sentir parte da jornada de Astro.",
+    link: "https://www.playstation.com/pt-br/games/astro-bot/",
+    ranking: "Top 5",
+},
+{
+    titulo: "Elden Ring: Sombra da Erdtree",
+    descricao: "O primeiro jogo da GameSpot a ganhar uma rara pontuação de 10/10 , em 2024 Shadow of the Erdtree é tudo o que você ama em Elden Ring e entregue com execução perfeita. Completando a grande história de Elden Ring, a expansão injeta novo conteúdo de jogabilidade, batalhas de chefes ainda mais difíceis e locais mais inspiradores em um jogo já excelente.",
+    link: "https://store.steampowered.com/app/2778580/ELDEN_RING_Shadow_of_the_Erdtree/",
+    ranking: "Top 4",
+
+},
+{
+    titulo: "Poço Animal ",
+    descricao: "Pode parecer um metroidvania bonitinho à primeira vista, mas quanto mais fundo você se aventura em Animal Well, mais surreal ele se torna, à medida que você se vê impelido a chegar ao fim. Criado pelo desenvolvedor solo Billy Basso, é um jogo de surpresas constantes, à medida que você se vê impelido a explorar mais uma sala em seu mundo encharcado de neon.",
+    link: "https://store.steampowered.com/app/813230/ANIMAL_WELL/",
+    ranking: "Top 3",
+},
+{
+    titulo : "Balatro",
+    descricao : "Quando você pensa que já viu todas as combinações possíveis do gênero roguelite, um jogo como Balatro aparece para entregar uma surpresa agradável. Rodadas rápidas de pôquer combinadas com mecânicas profundas de construção de deck e camadas de complexidade sob sua fachada enganosamente simples, Balatro vai all-in em seu conceito e é difícil de largar depois que ele crava suas presas em seu tempo livre.",
+    link : "https://store.steampowered.com/app/2379780/Balatro/",
+    ranking: "Top 2",
+},
+{
+    titulo : "Destiny 2: The Final Shape",
+    descricao : "Houve muita pressão sobre a Bungie para entregar uma conclusão satisfatória para a Saga Luz e Escuridão de Destiny, mas The Final Shape não apenas correspondeu a essas expectativas, como também acertou o pouso com estilo. Uma história emocionante, mudanças de jogabilidade impactantes e um final que uniu sua comunidade ajudaram a levar Destiny 2 a novos patamares.",
+    link : "https://www.destinythegame.com/pt_br/the-final-shape",
+    ranking: "Top 1",
+}];
